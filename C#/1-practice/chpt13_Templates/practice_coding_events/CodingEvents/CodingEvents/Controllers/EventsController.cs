f9e2b135c41158c0aa3bd07d72d321b9e44b3768
Queue <elbertq2@gmail.com>
2020-11-12 20:51:22 -0600

    Practice complete

---
+++ C#/1-practice/chpt13_Templates/practice_coding_events/CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Events.Add("Party in my Living Room");
+            //Events.Add("Party in my Kitchen");
+            //Events.Add("Party in my Garden Room");
+            ViewBag.events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+        }
+    }
+}
